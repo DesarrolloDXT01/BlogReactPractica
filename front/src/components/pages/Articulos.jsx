493454d760201b5df873affae4acb7f5b531daa1
EdgarMtzSndvl <desarrollo_01@dxt.com.mx>
2024-07-15 11:57:13 -0600

    peticiones y helpers

--- front/src/components/pages/Articulos.jsx
+++ front/src/components/pages/Articulos.jsx
@@ -1,41 +1,47 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { Global } from "../../helpers/Global";
+import { Peticion } from "../../helpers/Peticion";
 
 export const Articulos = () => {
+  const [articulos, setArticulos] = useState([]);
+  const [cargando, setCargando] = useState(true);
+
+  useEffect(() => {
+    getArticulos();
+  }, []);
+
+  const getArticulos = async () => {
+    const url = Global.url + "articulos";
+    const { datos, cargando } = await Peticion(url, "GET");
+
+    if (datos.status === "success") {
+      setArticulos(datos.articulos);
+    }
+    setCargando(false);
+  };
   return (
     <>
-     <article className="articulo-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">victorroblesweb.es</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-
-            <article className="articulo-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">victorroblesweb.es</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-
-            <article className="articulo-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">victorroblesweb.es</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-
-            <article className="articulo-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">victorroblesweb.es</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-
+      {cargando ? (
+        "Cargando...."
+      ) : articulos.length >= 1 ? (
+        articulos.map((articulo) => (
+          <article key={articulo._id} className="articulo-item">
+            <div className="mascara">
+              <img src="https://picsum.photos/1000" />
+            </div>
+            <div className="datos">
+              <h3 className="title">{articulo.titulo}</h3>
+              <p className="description">{articulo.contenido}</p>
+              <button className="edit">Editar</button>
+              <button className="delete">Borrar</button>
+            </div>
+          </article>
+        ))
+      ) : (
+        <h1>no hay articulos</h1>
+      )}
     </>
-  )
-}
-export default Articulos
+  );
+};
+export default Articulos;
--- front/src/components/pages/Inicio.jsx
+++ front/src/components/pages/Inicio.jsx
@@ -7,7 +7,7 @@ export const Inicio = () => {
       <h1>Blog con react</h1>
       <p> mern stack</p>
       <Link to="/articulos" className='button'> Ver los articulos</Link>
-      </div>
+    </div>
   )
 }
 
