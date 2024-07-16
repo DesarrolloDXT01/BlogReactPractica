ee6c20f9f3e6126382601abe67e823c8757d9cd5
EdgarMtzSndvl <desarrollo_01@dxt.com.mx>
2024-07-16 14:16:05 -0600

    estilos y inicio Formulario

--- front/src/components/pages/Crear.jsx
+++ front/src/components/pages/Crear.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import useForm from '../../hooks/useForm'
 
 export const Crear = () => {
+  const {formulario, enviado, cambiado}= useForm();
+
+  const guardaArticulo = (e) =>{
+    e.preventDefault();
+    let nuevoArticulo = JSON.stringify(formulario)
+  }
+
   return (
-    <div>Crear</div>
+    <div className='jumbo'>Crear
+      <h1>Crear articulo</h1>
+      <form className='formulario' onSubmit={enviado}>
+
+        <div className='form-group'>
+          <label htmlFor='titulo'> Titulo</label>
+          <input type='text' name='titulo' onChange={cambiado}/>
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='contenido'> Contenido</label>
+          <textarea type='text' name='contenido'  onChange={cambiado}/>
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='file0'> Imagen</label>
+          <input type='file' name='file0' id='file'  onChange={cambiado}/>
+        </div>
+
+        <input type='submit' value='guardar' className='btn btn-success'/>
+      </form>
+    </div>
   )
 }
 
